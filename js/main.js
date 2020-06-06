204ac2bf4c834475dac339847dc661e5da948131
unknown <dmitriy1991dp@gmail.com>
2020-06-06 17:22:11 +0300

    Startup_Project

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+
+    }
+});
+
+//wow JS
+new WOW({ mobile: false }).init();
+
+// Fixed Header on Scroll
+window.onscroll = function () {
+    const scrollTop = document.documentElement.scrollTop;
+    if (window.innerWidth > 768) {
+        if (scrollTop > 70) {
+            document.querySelector(".navigation").classList.add("sticky");
+        } else {
+            document.querySelector(".navigation").classList.remove("sticky");
+        }
+    }
+
+}
