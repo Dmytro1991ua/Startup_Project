b50d9b3b19ef070fd34a8fd11293b6c9198d78ca
unknown <dmitriy1991dp@gmail.com>
2020-06-08 22:28:17 +0300

    tabs/changed some styles

--- js/main.js
+++ js/main.js
@@ -54,5 +54,40 @@ window.onscroll = function () {
             document.querySelector(".navigation").classList.remove("sticky");
         }
     }
-
 }
+
+//tabs
+
+//select elements to work with (all childrens of tabs links and further content )
+function showTabs() {
+    const tabs = document.querySelector(".latest-work__items").children;
+    console.log(tabs);
+    const tabsContent = document.querySelector(".gallery").children;
+
+    //loop through each tab link
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener("click", (event) => {
+            event.preventDefault();
+            //add active class to a clicked tab link
+            for (let j = 0; j < tabs.length; j++) {
+                tabs[j].classList.remove("active");
+            }
+            tabs[i].classList.add("active");
+            const target = tabs[i].getAttribute("data-target");//data attribute for tabs
+            console.log(target);
+
+            //loop through all children of gallery
+            for (let k = 0; k < tabsContent.length; k++) {
+                tabsContent[k].style.transform = "scale(0)";
+                tabsContent[k].style.display = "none";
+                if (target === tabsContent[k].getAttribute("data-id") || target === "all") {
+                    tabsContent[k].style.transform = "scale(1)";
+                    tabsContent[k].style.display = "block";
+                }
+            }
+        });
+    }
+}
+
+showTabs();
+
